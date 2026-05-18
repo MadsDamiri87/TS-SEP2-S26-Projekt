42827d5c55aba11e27feb83794d99315e72bc2ed
Emil Tejn Rasmussen <emil@rasie.dk>
2026-05-18 13:53:43 +0200

    Fix course card showing the right state

--- frontend/src/components/coursecard/CourseCard.jsx
+++ frontend/src/components/coursecard/CourseCard.jsx
@@ -1,96 +1,104 @@
-import { useNavigate } from "react-router-dom";
-import "./CourseCard.css";
-import { useEffect, useState } from "react";
-import { BuyPopup } from "../popup/buyform/BuyPopup.jsx";
-import { getCourseById } from "../../api/courseApi.js";
+    import { useNavigate } from "react-router-dom";
+    import "./CourseCard.css";
+    import { useEffect, useState } from "react";
+    import { BuyPopup } from "../popup/buyform/BuyPopup.jsx";
+    import { getCourseById } from "../../api/courseApi.js";
 
-export function CourseCard({
-                               courseId,
-                               title,
-                               shortDescription,
-                               price,
-                               isEnrolled
-                           }) {
-    const navigate = useNavigate();
+    export function CourseCard({
+                                   courseId,
+                                   title,
+                                   shortDescription,
+                                   price,
+                                   isEnrolled
+                               }) {
+        const navigate = useNavigate();
 
-    const [course, setCourse] = useState(null);
-    const [isBuyPopupOpen, setIsBuyPopOpen] = useState(false);
+        const [course, setCourse] = useState(null);
+        const[enrolled, setEnrolled] = useState(isEnrolled)
+        const [isBuyPopupOpen, setIsBuyPopOpen] = useState(false);
 
 
-    useEffect(() => {
-        async function fetchDetails() {
-            try {
-                const data = await getCourseById(courseId);
-                setCourse(data);
-            } catch (error) {
-                console.error("Error fetching course", error);
+        useEffect(() => {
+            async function fetchDetails() {
+                try {
+                    const data = await getCourseById(courseId);
+                    setCourse(data);
+                } catch (error) {
+                    console.error("Error fetching course", error);
+                }
             }
+
+            fetchDetails();
+        }, [courseId, isEnrolled]);
+
+        useEffect(() => {
+            setEnrolled(isEnrolled);
+        }, [isEnrolled]);
+
+        function handleReadMore() {
+            navigate(`/course/${courseId}/${title}`);
         }
 
-        fetchDetails();
-    }, [courseId]);
+        function handleBuyCourse(event) {
+            event.stopPropagation();
+            setIsBuyPopOpen(true);
+        }
 
-    function handleReadMore() {
-        navigate(`/course/${courseId}/${title}`);
-    }
+        return (
+            <>
+                <div className="course-card" onClick={handleReadMore}>
+                    <h3 className="course-card-title">
+                        {title}
+                    </h3>
 
-    function handleBuyCourse(event) {
-        event.stopPropagation();
-        setIsBuyPopOpen(true);
-    }
-
-    return (
-        <>
-            <div className="course-card" onClick={handleReadMore}>
-                <h3 className="course-card-title">
-                    {title}
-                </h3>
-
-                <p className="course-card-description">
-                    {shortDescription}
-                </p>
-
-                {!isEnrolled && (
-                    <p className="course-card-price">
-                        {"$" + price}
+                    <p className="course-card-description">
+                        {shortDescription}
                     </p>
-                )}
 
-                <div className="course-card-actions">
-                    {isEnrolled ? (
-                        <div className="continue-learning">
-                            <span className="continue-text">
-                                Continue Course
-                            </span>
-
-                            <span className="continue-arrow">
-                                →
-                            </span>
-                        </div>
-                    ) : (
-                        <>
-                            <button type="button" onClick={handleReadMore}>
-                                Read more
-                            </button>
-
-                            <button
-                                type="button"
-                                className="highlight-btn"
-                                onClick={handleBuyCourse}
-                            >
-                                Purchase
-                            </button>
-                        </>
+                    {!enrolled && (
+                        <p className="course-card-price">
+                            {"$" + price}
+                        </p>
                     )}
-                </div>
-            </div>
 
-            {isBuyPopupOpen && course && (
-                <BuyPopup
-                    course={course}
-                    onClose={() => setIsBuyPopOpen(false)}
-                />
-            )}
-        </>
-    );
-}
+                    <div className="course-card-actions">
+                        {enrolled ? (
+                            <div className="continue-learning">
+                                <span className="continue-text">
+                                    Continue Course
+                                </span>
+
+                                <span className="continue-arrow">
+                                    →
+                                </span>
+                            </div>
+                        ) : (
+                            <>
+                                <button type="button" onClick={handleReadMore}>
+                                    Read more
+                                </button>
+
+                                <button
+                                    type="button"
+                                    className="highlight-btn"
+                                    onClick={handleBuyCourse}
+                                >
+                                    Purchase
+                                </button>
+                            </>
+                        )}
+                    </div>
+                </div>
+
+                {isBuyPopupOpen && course && (
+                    <BuyPopup
+                        course={course}
+                        onClose={() => {
+                            setIsBuyPopOpen(false)
+                            setEnrolled(true)
+                        }}
+                    />
+                )}
+            </>
+        );
+    }
